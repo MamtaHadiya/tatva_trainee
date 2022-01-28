c54c5734a07a070aec358748e6c19abb83819bee
MamtaHadiya <hadiyamamta4590@gmail.com>
2022-01-28 15:53:55 +0530

    add Helperland

---
+++ Helperland/Controllers/HelperlandController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.Controllers
+{
+    public class HelperlandController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index", "_Layout");
+        }
+        public IActionResult Prices()
+        {
+            return View("Prices", "_Layout1");
+        }
+    }
+}
